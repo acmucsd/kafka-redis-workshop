1b610d64a00d5b7d3cd7cc1185ff82a1eb370a30
Nikhil Dange <nsdange@ucsd.edu>
2024-04-30 16:36:41 -0700

    updated money service algo

--- services/moneyService.js
+++ services/moneyService.js
@@ -1,5 +1,7 @@
 const getCostForOrder = (fromLocation, toLocation, orderDetails) => {
     // advanced algorithm to calculate the cost
+    // leverages blockchain, big data, machine learning, and generative AI to produce an optimal value.
+    // venture capitalists please reach out at nsdange@ucsd.edu
     const price = (Math.random() * 20) + 10;
     const priceRoundedToTwoDecimals = Math.round(price * 100) / 100;
     return priceRoundedToTwoDecimals
